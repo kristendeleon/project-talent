4bd67b612364999cfdfe83a61c696295b6f8354a
kristen de leon <kristenssendeleon@gmail.com>
2025-04-17 14:36:04 +1200

    display jobs card

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -1,6 +1,18 @@
 ﻿import React from 'react';
 import Cookies from 'js-cookie';
-import { Popup } from 'semantic-ui-react';
+import {
+    Popup,
+    CardMeta,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    Card,
+    Label,
+    ButtonGroup,
+    Button,
+    Icon
+   
+} from 'semantic-ui-react';
 import moment from 'moment';
 
 export class JobSummaryCard extends React.Component {
@@ -8,12 +20,41 @@ export class JobSummaryCard extends React.Component {
         super(props);
         this.selectJob = this.selectJob.bind(this)
     }
-
     selectJob(id) {
         var cookies = Cookies.get('talentAuthToken');
         //url: 'http://localhost:51689/listing/listing/closeJob',
     }
-
     render() {
+        const job = this.props.job ? this.props.job : " ";
+        const city = job.location.city != 0 ? job.location.city + "," : " ";
+        return (
+            <div>
+                
+                    <Card>
+                        <CardContent>
+                            <CardHeader>{job.title}</CardHeader>
+                            <Label color='black' ribbon='right'>
+                                <Icon name="user"></Icon>
+                                0
+                            </Label>
+                            <CardMeta>{city} {job.location.country}</CardMeta>
+                            <CardDescription>
+                                {job.summary}
+                            </CardDescription>
+                        </CardContent>
+                        <CardContent extra>
+                            <div className="card-action-btn">
+                                <Button compact color='red' floated="left" className="card-btn">Expired</Button>
+                                <ButtonGroup>
+                                    <Button compact basic color='blue' className="card-btn"><Icon name='ban'></Icon>Close</Button>
+                                    <Button compact basic color='blue' className="card-btn"><Icon name='edit outline'></Icon>Edit</Button>
+                                    <Button compact basic color='blue' className="card-btn"><Icon name='copy outline'></Icon>Copy</Button>
+                                </ButtonGroup>
+                            </div>
+                        </CardContent>
+                    </Card>
+                   
+            </div>
+        );
     }
 }
